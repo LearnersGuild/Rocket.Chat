c327391f94c1f17752ba44f2bf0ae948a5543c29
Sabra Pratt <pratt.sabra@gmail.com>
2016-06-21 15:57:18 -0700

    update `update.sh` script

--- learners-guild/update.sh
+++ learners-guild/update.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
-echo "Updating Echo Chat Dependencies"
+echo "\nUpdating Echo Chat Dependencies"
 
-echo "Git: Pulling repos"
-echo "Git: Pulling rocketchat-ls-sso"
-cd ../rocketchat-lg-sso
-git pull origin master
-
-echo "Git: Pulling rocketchat-ls-slash-commands"
-cd ../rocketchat-lg-slash-commands
-git pull origin master
-
-echo "Git: Pulling rocketchat-lg-api-extensions"
+echo "\nUpdating local copies of packages"
 cd ../rocketchat-lg-api-extensions
 git pull origin master
 
-echo "Meteor: Removing packages"
+cd ../rocketchat-lg-sso
+git pull origin master
+
+cd ../rocketchat-lg-slash-commands
+git pull origin master
+
+echo "\nRemoving meteor packages"
 cd ../echo-chat
-echo "Meteor: Removing learnersguild:rocketchat-lg-sso"
+meteor remove learnersguild:rocketchat-lg-api-extensions
 meteor remove learnersguild:rocketchat-lg-sso
-echo "Meteor: Removing learnersguild:rocketchat-lg-sso"
 meteor remove learnersguild:rocketchat-lg-slash-commands
 
-echo "Creating package symlinks"
+echo "\nCreating package symlinks"
 cd packages
+ln -s ../../rocketchat-lg-api-extensions .
 ln -s ../../rocketchat-lg-sso .
 ln -s ../../rocketchat-lg-slash-commands .
 
-echo "Meteor: Adding packages"
+echo "\nAdding meteor packages"
 cd ..
-echo "Meteor: Adding learnersguild:rocketchat-lg-sso"
+meteor add learnersguild:rocketchat-lg-api-extensions
 meteor add learnersguild:rocketchat-lg-sso
-echo "Meteor: Adding learnersguild:rocketchat-lg-slash-commands"
 meteor add learnersguild:rocketchat-lg-slash-commands
